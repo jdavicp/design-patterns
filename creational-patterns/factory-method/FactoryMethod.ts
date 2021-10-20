4c1730eb918b6f7ee2af001ebe5f7de6a5b433bf
jdavicp <jdavi.fac@gmail.com>
2021-10-20 17:28:30 -0300

    Exemplo de factory method

--- creational-patterns/factory-method/FactoryMethod.ts
+++ creational-patterns/factory-method/FactoryMethod.ts
@@ -1,42 +1,36 @@
-interface  FoodDelivery {
-    deliver(): Food;
-    payment(): number;
-    
-}
 
-abstract class Food {
+class Food {
     private price: number;
-    constructor(price: number) {
+    private ingredients: string[];
+    private name: string;
+
+    constructor(name: string, price: number, ingredients: string[]) {
         this.price = price;
+        this.ingredients = ingredients;
+        this.name = name;
     }
-    getprice(): number {
-        return this.price
+    getPrice(): number {
+        return this.price;
     };
-    abstract ingredients(): string[];
-}
-
-class Acai extends Food {
-    constructor() {
-        super(11.50);
-    }
-    ingredients(): string[] {
-        return ['poupa de açaí', 'banana', 'morango', 'mel'];
-    }
-    
-}
-class BikeDelivery implements FoodDelivery{
-    
-    deliver(): Food {
-        return new Acai();
-    }
-    payment(): number {
-        return this.deliver().getprice() + 5.00;
+    getIngredients(): string[] {
+        return this.ingredients
+    };
+    getName(): string {
+        return this.name;
     }
 
+    food(): Food {
+        return new Food(this.getName(), this.getPrice(), this.getIngredients());
+    }
 }
 
-let pedido = new BikeDelivery().deliver();
-console.log(pedido);
+const acai = new Food('Açaí', 10.45,['poupa de açaí', 'banana', 'morango', 'mel']).food();
+const bolo = new Food('Bolo', 25.36,['chocolate', 'farinha de trigo', 'ovos', 'fermento', 'açucar']).food();
+
+console.log(acai);
+console.log(bolo);
+
+
 
 
 
