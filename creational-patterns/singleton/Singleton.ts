caf72e9099a97db42793188404cd3f5d3044884b
jdavicp <jdavi.fac@gmail.com>
2021-10-22 12:18:49 -0300

    Exemplo singleton

---
+++ creational-patterns/singleton/Singleton.ts
@@ -0,0 +1,17 @@
+class Singleton {
+    private static instance: Singleton;
+    private constructor() {}
+
+    public static getInstance(): Singleton {
+        if(!Singleton.instance) {
+            Singleton.instance = new Singleton();
+        }
+        return Singleton.instance;
+    }
+
+    public someOperation(): void {}
+}
+
+const instance1 = Singleton.getInstance();
+const instance2 = Singleton.getInstance();
+console.log(instance1 == instance2);
